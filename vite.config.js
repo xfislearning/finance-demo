f5648e94707222997bb524824f3a9d116981e011
xfislearning <sophieyangnc@gmail.com>
2026-09-01 20:47:39 -0600

    fix 404 error

--- vite.config.js
+++ vite.config.js
@@ -1,10 +1,7 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-export default defineConfig(() => {
-  const repo = process.env.GITHUB_REPOSITORY?.split('/')[1]
-  return {
-    plugins: [react()],
-    base: process.env.GITHUB_ACTIONS && repo ? `/${repo}/` : '/',
-  }
+export default defineConfig({
+  plugins: [react()],
+  base: '/',
 })
